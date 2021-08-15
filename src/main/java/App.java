bb485aea40b507175fdbc6b3b2327e7f4f098fef
Admin <real282@mail.ru>
2021-08-15 17:35:11 +0800

    На проверку

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = applicationContext.getBean(Cat.class);
+        Cat beanCat2 = (Cat) applicationContext.getBean("Cat");
+
+        System.out.println(bean1 == bean2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
